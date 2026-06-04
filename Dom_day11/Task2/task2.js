cab280a8697319c49a492d849d960a86f4045f45
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-06-04 11:37:12 +0530

    Updated DOM tasks and Day13

--- Dom_day11/Task2/task2.js
+++
@@ -1,9 +0,0 @@
-let GetSumbtn = document.querySelector("#sumbtn")
-GetSumbtn.addEventListener("click",()=>{
-    let val1=Number(document.querySelector("#val1").value)
-    let val2=Number(document.querySelector("#val2").value)
-    let para=document.querySelector("#para")
-    para.textContent=`sum is ${val1+val2}`
-    document.querySelector("#val1").value=""
-    document.querySelector("#val2").value=""
-})
